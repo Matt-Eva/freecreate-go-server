fc88bd7d9033b94011e65205d66b634d14c20ef5
Matt Eva <matteweva@gmail.com>
2025-06-07 21:47:57 -0700

    comenting out cors

--- main.go
+++ main.go
@@ -2,30 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	router := gin.Default()
 	store := cookie.NewStore([]byte("secret"))
 	router.Use(sessions.Sessions("mysession", store))
-	router.Use(cors.New(cors.Config{
-		AllowOrigins: []string{}
-	}))
 
-	router.GET("/session", func(c *gin.Context){
+	// allowedOrigin := os.Getenv("CLIENT_ORIGIN")
+	// router.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{allowedOrigin},
+	// 	AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           1 * time.Hour,
+	// }))
+
+	router.GET("/session", func(c *gin.Context) {
 		session := sessions.Default(c)
 
 		sessionId := session.Get("sessionId")
 
 		fmt.Println(sessionId)
-		
+
 	})
 
-	router.POST("/sessions", func (c *gin.Context){})
+	router.POST("/sessions", func(c *gin.Context) {
+		session := sessions.Default(c)
+		session.Set("sessionId", "myId")
+		session.Save()
 
-	router.DELETE("/sessions", func(c *gin.Context){})
+		c.JSON(201, gin.H{"sessionId": "myId"})
+	})
+
+	router.DELETE("/sessions", func(c *gin.Context) {
+		session := sessions.Default(c)
+		session.Delete("sessionId")
+		session.Save()
+
+		c.JSON(200, gin.H{"sessionId": false})
+	})
+
+	router.Run()
 }
