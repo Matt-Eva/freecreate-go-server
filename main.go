c29ab7e565be64a15139e7d0213f2ba3615aa8cb
Matt Eva <matteweva@gmail.com>
2025-07-05 10:30:59 -0700

    readded dev cors function

--- main.go
+++ main.go
@@ -11,17 +11,37 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func devCorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		allowedOrigin := os.Getenv("CLIENT_ORIGIN")
+
+		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		w.Header().Set("Access-Contol-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Max-Age", "86400")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	environment := os.Getenv("ENVIRONMENT")
+
+	router := mux.NewRouter()
 
 	if environment != "PRODUCTION" {
 		err := godotenv.Load()
 		if err != nil {
 			log.Fatal("Error loading .env file")
 		}
+		
 	}
 
-	router := mux.NewRouter()
+	
 
 	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("hit route")
