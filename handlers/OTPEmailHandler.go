d7dbf5af0400997eee72f43874a0eb6931eee509
Matt Eva <matteweva@gmail.com>
2025-08-19 14:26:24 -0500

    created otp generator

---
+++ handlers/OTPEmailHandler.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"net/http"
+	"strconv"
+
+	"github.com/resend/resend-go/v2"
+)
+
+func OTPEmailHandler(resendClient *resend.Client) http.HandlerFunc{
+	return func (w http.ResponseWriter, r *http.Request){
+
+	var otp string;
+	for i := 0; i < 8; i++ {
+		a, _ := rand.Int(rand.Reader, big.NewInt(10))
+		str := strconv.FormatInt(a.Int64(), 10)
+		otp += str
+	}
+	fmt.Println(otp)
+
+	}
+}
