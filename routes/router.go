6decd8d5fa065a5e50e51c30e6226a7fe8725f5f
Matt Eva <matteweva@gmail.com>
2026-06-16 13:16:18 -0700

    removed unused test code and commented out code

--- routes/router.go
+++ routes/router.go
@@ -1,9 +1,7 @@
 package routes
 
 import (
-	"encoding/json"
 	"fmt"
-	"freecreate/logger"
 	"freecreate/middleware"
 	"freecreate/web_api_handlers"
 	"freecreate/web_page_handlers"
@@ -64,7 +62,7 @@ func CreateRouter(sessionStore *sessions.CookieStore, gormPGClient *gorm.DB, mon
 
 	router.Get("/donate", web_page_handlers.DonatePageHandler(templates))
 
-// ======== JSON Web API Routes =======
+// ======== JSON Web API Routes =========
 	router.Route("/web-api", func(r chi.Router) {
 
 		router.Post("/login", web_api_handlers.LoginHandler(sessionStore, gormPGClient))
@@ -95,85 +93,8 @@ func CreateRouter(sessionStore *sessions.CookieStore, gormPGClient *gorm.DB, mon
 
 		// router.Post("/email", handlers.EmailHandler(resendClient))
 
-		router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("hit route hello")
-
-			type Response struct {
-				Message string `json:"message"`
-			}
-
-			response := Response{
-				Message: "Hello world!",
-			}
-
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(response)
-		})
-
-		router.Post("/hello", func(w http.ResponseWriter, r *http.Request) {
-			type Body struct {
-				Message string `json:"message"`
-			}
-
-			var body Body
-
-			jErr := json.NewDecoder(r.Body).Decode(&body)
-			if jErr != nil {
-				logger.Log(jErr)
-				http.Error(w, jErr.Error(), http.StatusUnprocessableEntity)
-				return
-			}
-
-			fmt.Println(body.Message)
-
-			type Response struct {
-				Message string `json:"message"`
-			}
-
-			response := Response{
-				Message: body.Message,
-			}
-
-			res, mErr := json.Marshal(response)
-			if mErr != nil {
-				logger.Log(mErr)
-				http.Error(w, mErr.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			w.Write(res)
-		})
-
 	})
 
 	return router
 }
 
-// router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Println("hit route")
-
-// 		type Response struct {
-// 			Message string `json:"message"`
-// 		}
-
-// 		response := Response{
-// 			Message: "Hello world!",
-// 		}
-
-// 		w.WriteHeader(http.StatusOK)
-// 		json.NewEncoder(w).Encode(response)
-// 	}).Methods("GET")
-
-// 	router.HandleFunc("/session", func(w http.ResponseWriter, r *http.Request) {
-
-// 	}).Methods("GET")
-
-// 	router.HandleFunc("/session", func(w http.ResponseWriter, r *http.Request) {
-
-// 	}).Methods("POST")
-
-// 	router.HandleFunc("/session", func(w http.ResponseWriter, r *http.Request) {
-
-// 	}).Methods("DELETE")
