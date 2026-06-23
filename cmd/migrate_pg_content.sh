ced2a0b67d5529cbc15d475df231ee22ec7d1407
Matt Eva <matteweva@gmail.com>
2026-06-23 16:11:27 -0700

    successfully created commands for migrations

--- cmd/migrate_pg_content.sh
+++ cmd/migrate_pg_content.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "running pg content migration"
+
+
+dbmate -d "./db/pg_content/migrations" -s "./db/pg_content/schema.sql" --url "postgres://matte:code@localhost:5432/freecreate_go_writing_content?sslmode=disable" migrate
--- cmd/migrate_pg_core.sh
+++ cmd/migrate_pg_core.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "running pg core migration"
+
+dbmate -d "./db/pg_core/migrations" -s "./db/pg_core/schema.sql" --url "postgres://matte:code@localhost:5432/freecreate_go?sslmode=disable" migrate
