627e74da27280061b8ee097ff64bc86b7489269a
Matt Eva <matteweva@gmail.com>
2026-07-14 09:00:43 -0700

    added redirect to profile route for unauthorized users

--- web_page_handlers/profile_page_handler.go
+++ web_page_handlers/profile_page_handler.go
@@ -1,13 +1,21 @@
 package web_page_handlers
 
 import (
+	"freecreate/auth"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
 
-func ProfilePageHandler(profileTmpl *template.Template) http.HandlerFunc {
+func ProfilePageHandler(sessionStore *sessions.CookieStore,profileTmpl *template.Template) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
+		_, getUserErr := auth.GetUser(sessionStore, w, r)
+		if getUserErr != nil {
+			http.Redirect(w, r, "/login", 303)
+			return
+		}
 		type PageData struct {
 			LoggedIn bool
 		}
