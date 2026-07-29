2cec3834cce8e1c1f69b3f3b49162d733ae118f7
Matt Eva <matteweva@gmail.com>
2026-07-28 19:04:42 -0700

    playing with CSS card display styles

--- static/globals.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("running globals js");
-
-  const metaLoggedIn = document.getElementById("meta_logged_in");
-
-  console.log(metaLoggedIn);
-});
