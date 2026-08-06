82b84fea02208154ae5f4bb4be0266f767803613
Matt Eva <matteweva@gmail.com>
2026-08-06 07:43:14 -0700

    removing profanity.go, which contained prohibited words

--- internal/config/profanity.go
+++
@@ -1,7 +0,0 @@
-package config
-
-// Hello! If you're reading through this code base, please know that the following words are only written out
-// so as to prevent their use in people's names, tags, and titles. We do not allow hate speech, and do not allow
-// the following words / slurs in people's creator names, titles, tags, or descriptions.
-
-// var profanity = []string{"nigger", "nigga", "niglet", "rape", "gook", "beaner", ""}
