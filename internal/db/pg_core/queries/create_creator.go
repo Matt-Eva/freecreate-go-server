d8fb717a8fbffde368303f48be8861317f060561
Matt Eva <matteweva@gmail.com>
2026-08-10 19:38:15 -0700

    saving create creator progress

--- internal/db/pg_core/queries/create_creator.go
+++ internal/db/pg_core/queries/create_creator.go
@@ -1,16 +1,55 @@
 package pg_core_queries
 
 import (
+	"context"
 	"freecreate/internal/config"
+	pg_core_validators "freecreate/internal/db/pg_core/validators"
 	"freecreate/internal/lib/api_error"
+	"freecreate/internal/lib/logger"
+	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type NewCreator struct{
-
+type CreatedCreator struct{
+	Name string
+	UUID uuid.UUID
 }
 
-func CreateCreator(pgCore *pgxpool.Pool, pgCoreQueries config.PgCoreQueries)(newCreator, *api_error.Error){
+func CreateCreator(ctx context.Context, pgCore *pgxpool.Pool, pgCoreQueries config.PgCoreQueries, creatorName string, userId int64)(CreatedCreator, *api_error.Error){
 	query := pgCoreQueries.CreateCreator()
+	namedArgs := pgx.NamedArgs{
+		"name": creatorName,
+		"user_id": userId,
+	}
+
+	validateCreatorErr := pg_core_validators.ValidateCreator(namedArgs)
+	if validateCreatorErr != nil {
+		return newCreator{}, validateCreatorErr
+	}
+
+	var name string
+	var uuid uuid.UUID
+
+	createCreatorErr := pgCore.QueryRow(ctx, query, namedArgs).Scan(&name, &uuid)
+	if createCreatorErr != nil {
+		logger.Log(createCreatorErr)
+
+		apiErr := &api_error.Error{
+			Code: http.StatusInternalServerError,
+			Message: api_error.InteralServerErrorMessage,
+			Error: createCreatorErr,
+		}
+
+		return newCreator{}, apiErr
+	}
+
+	newCreator := newCreator{
+		Name: name,
+		UUID: uuid,
+	}
+
+	return newCreator, nil
 }
--- internal/db/pg_core/validators/validate_creator.go
+++
@@ -1,5 +0,0 @@
-package pg_core_validators
-
-func ValidateCreator(){
-	
-}
