0051b3f168d80580f5f4a6e1cd6e31354a5e7013
Matt Eva <matteweva@gmail.com>
2026-08-13 16:58:54 -0700

    began fleshing out create writing migration

--- internal/db/pg_core/migrations/20260813232323_create_writing_table.sql
+++ internal/db/pg_core/migrations/20260813232323_create_writing_table.sql
@@ -3,26 +3,35 @@ CREATE TABLE writings (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
     creator_id BIGINT NOT NULL,
-    uuid DEFAULT gen_random_uuid(),
+    uuid UUID DEFAULT gen_random_uuid(),
     title VARCHAR(200) NOT NULL,
-    genres ARRAY,
-    tags ARRAY, 
-    rank BIGINT default 0,
-    rel_rank BIGINT default 0,
+    subtitle VARCHAR(200),
+    genres TEXT ARRAY,
+    tags TEXT ARRAY, 
+    rank BIGINT DEFAULT 0,
+    rel_rank BIGINT DEFAULT 0,
     description TEXT,
-    views BIGINT default 0,
-    list_adds BIGINT default 0,
-    likes BIGINT default 0,
-    lib_adds BIGINT default 0,
-    donations BIGINT default 0,
-    flags BIGINT default 0,
-    FOREIGN KEY (user_id) REFERENCES users.id ON DELETE CASCADE,
-    FOREIGN KEY (creator_id) REFERENCES creators.id ON DELETE CASCADE
+    views BIGINT DEFAULT 0,
+    list_adds BIGINT DEFAULT 0,
+    likes BIGINT DEFAULT 0,
+    lib_adds BIGINT DEFAULT 0,
+    donations BIGINT DEFAULT 0,
+    flags BIGINT DEFAULT 0,
+    created_at TIMESTAMP,
+    published BOOLEAN,
+    published_before BOOLEAN,
+    last_published TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (creator_id) REFERENCES creators(id) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_writings_uuid ON writings.uuid,
-CREATE INDEX idx_writings_user_id ON writings.user_id,
-CREATE INDEX idx_writings_creator_id ON writings.creator_id
+CREATE INDEX idx_writings_uuid ON writings(uuid);
+CREATE INDEX idx_writings_user_id ON writings(user_id);
+CREATE INDEX idx_writings_creator_id ON writings(creator_id);
+CREATE INDEX idx_writings_rank ON writings(rank);
+CREATE INDEX idx_writings_rel_rank ON writings(rel_rank);
+CREATE INDEX idx_writings_created_at ON writings(created_at);
+CREATE INDEX idx_writings_last_published ON writings(last_published);
 
 -- migrate:down
 DROP TABLE writings;
